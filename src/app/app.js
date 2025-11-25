a0fe7a381c58567802fdc71327a2ca7ff1719f23
Ahmad Alabed <you@example.comengahmadalabed@gmail.com>
2025-11-25 12:01:21 +0300

    feat: implement app initialization logic with state, filtering, rendering, and form handling

---
+++ src/app/app.js
@@ -0,0 +1,50 @@
+import { expensesState } from "../state/expenses.js";
+import { renderTable } from "../ui/table/renderer.js";
+import { createExpense, calculateTotalAmount } from "../core/expense/model.js";
+import { attachTableEvents } from "../ui/table/events.js";
+import { createFormController } from "../ui/form/controller.js";
+import { createCategoryFilter } from "../ui/filter/category.js";
+
+export function init(form, tbody, totalAmountEl, categorySelect) {
+  const expenses = expensesState([]);
+  let currentCategory = "";
+  updateUI();
+  attachTableEvents(tbody, (id) => {
+    expenses.remove(id);
+    updateUI();
+  });
+
+  createCategoryFilter(categorySelect, (category) => {
+    currentCategory = category;
+    updateUI();
+  });
+  const formController = createFormController(form);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (!formController.validate()) return;
+    const expense = createExpense({
+      title: form.title.value,
+      amount: form.amount.value,
+      category: form.category.value,
+      date: form.date.value,
+    });
+
+    expenses.add(expense);
+    form.reset();
+
+    updateUI();
+  });
+
+  function updateUI() {
+    let data = expenses.getAll();
+    if (currentCategory) {
+      data = data.filter((e) => e.category === currentCategory);
+    }
+    const rows = renderTable(data);
+    const fragment = document.createDocumentFragment();
+    rows.forEach((row) => fragment.appendChild(row));
+    tbody.innerHTML = "";
+    tbody.appendChild(fragment);
+    totalAmountEl.textContent = `$ ${calculateTotalAmount(data).toFixed(2)}`;
+  }
+}
